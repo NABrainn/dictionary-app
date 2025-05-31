7b6e9b7a50bcc744927e85bd4d2c99e8569061ea
NAbrain <michal2002.60@gmail.com>
2025-05-31 19:20:24 +0200

    add method

--- src/main/java/lule/dictionary/exception/RepositoryException.java
+++ src/main/java/lule/dictionary/exception/RepositoryException.java
@@ -8,4 +8,7 @@ public class RepositoryException extends RuntimeException {
     public RepositoryException(String message, Throwable cause) {
         super(message, cause);
     }
+    public RepositoryException(Throwable cause) {
+        super(cause);
+    }
 }
