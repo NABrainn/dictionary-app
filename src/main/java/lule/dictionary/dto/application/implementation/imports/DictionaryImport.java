940b8733a8ce06f22f16384e4650af7b07562ea9
NAbrain <michal2002.60@gmail.com>
2025-06-01 10:37:59 +0200

    hide awat sensitive user information from dtos

--- src/main/java/lule/dictionary/dto/application/implementation/imports/DictionaryImport.java
+++ src/main/java/lule/dictionary/dto/application/implementation/imports/DictionaryImport.java
@@ -6,11 +6,15 @@ import lombok.extern.slf4j.Slf4j;
 import lule.dictionary.dto.application.interfaces.imports.Import;
 import lule.dictionary.dto.application.interfaces.imports.ImportDetails;
 import lule.dictionary.dto.application.interfaces.userProfile.UserProfile;
+import lule.dictionary.dto.application.interfaces.userProfile.UserProfileSettings;
 
 @Slf4j
 public record DictionaryImport(
         @NonNull
         ImportDetails importDetails,
         @NonNull
-        UserProfile userProfile) implements Import {
+        UserProfileSettings userProfileSettings,
+        @NonNull
+        String owner) implements Import {
+
 }
