de4b2bcc87508557b3dc85f4fe40c0da3cd54e22
NAbrain <michal2002.60@gmail.com>
2025-06-01 18:13:12 +0200

    add missing api, decorate with nonnull

--- src/main/java/lule/dictionary/service/application/dto/UserProfileService.java
+++ src/main/java/lule/dictionary/service/application/dto/UserProfileService.java
@@ -1,5 +1,6 @@
 package lule.dictionary.service.application.dto;
 
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lule.dictionary.dto.application.interfaces.userProfile.UserProfile;
 import lule.dictionary.repository.UserProfileRepository;
@@ -7,13 +8,15 @@ import lule.dictionary.exception.RepositoryException;
 import lule.dictionary.exception.ServiceException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserProfileService {
 
     private final UserProfileRepository userProfileRepository;
 
-    public UserProfile findByUsername(String username) throws ServiceException {
+    public UserProfile findByUsername(@NonNull String username) throws ServiceException {
         try {
             return userProfileRepository.findByUsername(username).orElseThrow(() -> new ServiceException("User not found"));
         } catch (RepositoryException e) {
@@ -21,9 +24,17 @@ public class UserProfileService {
         }
     }
 
-    public UserProfile addUserProfile(UserProfile userProfile) throws ServiceException {
+    public UserProfile addUserProfile(@NonNull UserProfile userProfile) throws ServiceException {
         try {
             return userProfileRepository.addUserProfile(userProfile.userProfileCredentials(), userProfile.userProfileSettings()).orElseThrow(() -> new ServiceException("User not found"));
+        } catch (RepositoryException e) {
+            throw new ServiceException("User not found");
+        }
+    }
+
+    public List<UserProfile> findAll() throws ServiceException {
+        try {
+            return userProfileRepository.findAll();
         } catch (RepositoryException e) {
             throw new ServiceException("User not found");
         }
