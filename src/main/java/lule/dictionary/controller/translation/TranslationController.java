a39aeb495d86327687be1d5819c4dadf45afe27d
NAbrain <michal2002.60@gmail.com>
2025-06-06 15:07:38 +0200

    update controller based return names

--- src/main/java/lule/dictionary/controller/translation/TranslationController.java
+++ src/main/java/lule/dictionary/controller/translation/TranslationController.java
@@ -26,7 +26,6 @@ public class TranslationController {
                                    @RequestParam int importId,
                                    @RequestParam String targetWord,
                                    @RequestParam("selectedWordId") int selectedWordId) {
-
             boolean found = translationService.findByTargetWord(model, new FindTranslationRequest(importId, targetWord, selectedWordId));
             if(found) {
                 return "translation/update-translation-form";
@@ -36,17 +35,16 @@ public class TranslationController {
 
     @PostMapping({"/new", "new"})
     public String newTranslation(Model model,
-            @RequestParam("sourceWord") String sourceWord,
-            @RequestParam("targetWord") String targetWord,
-            @RequestParam("familiarity") Familiarity familiarity,
-            @RequestParam("sourceLanguage") Language sourceLanguage,
-            @RequestParam("targetLanguage") Language targetLanguage,
-            @RequestParam("owner") String owner,
-            @RequestParam("importId") int importId,
-            @RequestParam("selectedWordId") int selectedWordId
-    ) {
+                                @RequestParam("sourceWord") String sourceWord,
+                                @RequestParam("targetWord") String targetWord,
+                                @RequestParam("familiarity") Familiarity familiarity,
+                                @RequestParam("sourceLanguage") Language sourceLanguage,
+                                @RequestParam("targetLanguage") Language targetLanguage,
+                                @RequestParam("owner") String owner,
+                                @RequestParam("importId") int importId,
+                                @RequestParam("selectedWordId") int selectedWordId) {
         translationService.add(model, new AddTranslationRequest(sourceWord, targetWord, familiarity, sourceLanguage, targetLanguage, owner, importId, selectedWordId));
-        return "forward:/imports/page/reload";
+        return "forward:/dictionary/page/reload";
     }
 
     @PutMapping({"/familiarity/update", "familiarity/update"})
@@ -61,6 +59,6 @@ public class TranslationController {
                                     @RequestParam("selectedWordId") int selectedWordId) {
 
         translationService.updateFamiliarity(model, new UpdateFamiliarityRequest(targetWord, familiarity, sourceWord, sourceLanguage, targetLanguage, owner, importId, selectedWordId));
-        return "forward:/imports/page/reload";
+        return "forward:/dictionary/page/reload";
     }
 }
