9de1f38c0af14b4f5cb0c4e6f96699f1e65645f6
NAbrain <michal2002.60@gmail.com>
2025-06-10 13:44:33 +0200

    implement UserDetails

--- src/main/java/lule/dictionary/dto/application/implementation/userProfile/DictionaryUserProfile.java
+++ src/main/java/lule/dictionary/dto/application/implementation/userProfile/DictionaryUserProfile.java
@@ -6,10 +6,49 @@ import lombok.NonNull;
 import lule.dictionary.dto.application.interfaces.userProfile.UserProfile;
 import lule.dictionary.dto.application.interfaces.userProfile.UserProfileCredentials;
 import lule.dictionary.dto.application.interfaces.userProfile.UserProfileSettings;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Builder
 public record DictionaryUserProfile(
         @NonNull
         UserProfileCredentials userProfileCredentials,
-        UserProfileSettings userProfileSettings) implements UserProfile {
+        UserProfileSettings userProfileSettings) implements UserProfile, UserDetails {
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+                return List.of();
+        }
+
+        @Override
+        public String getPassword() {
+                return userProfileCredentials.password();
+        }
+
+        @Override
+        public String getUsername() {
+                return userProfileCredentials.username();
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+                return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+                return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+                return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+                return true;
+        }
 }
