5734ae42031fab3b37241d3efd61465358554cdb
NAbrain <michal2002.60@gmail.com>
2025-06-10 13:53:42 +0200

    update Model

---
+++ src/main/java/lule/dictionary/controller/catalog/dto/CatalogModel.java
@@ -0,0 +1,18 @@
+package lule.dictionary.controller.catalog.dto;
+
+import lombok.NonNull;
+import lule.dictionary.dto.application.interfaces.translation.Translation;
+
+import java.util.List;
+import java.util.Map;
+
+public record CatalogModel(@NonNull String title, List<String> content, Map<String, Translation> translations, int importId, int selectedWordId) {
+    @Override
+    public List<String> content() {
+        return List.copyOf(content);
+    }
+    @Override
+    public Map<String, Translation> translations() {
+        return Map.copyOf(translations);
+    }
+}
