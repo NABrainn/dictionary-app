e229277ebcd1da190539ebf20b16759527bd9c2f
NAbrain <michal2002.60@gmail.com>
2025-06-10 13:41:57 +0200

    implement exception type for validation purposes

---
+++ src/main/java/lule/dictionary/service/auth/validator/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package lule.dictionary.service.auth.validator.exception;
+
+public class ValidationException extends RuntimeException{
+    public ValidationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/lule/dictionary/service/auth/validator/exception/ValidationStrategyException.java
@@ -0,0 +1,7 @@
+package lule.dictionary.service.auth.validator.exception;
+
+public class ValidationStrategyException extends RuntimeException{
+    public ValidationStrategyException(String s) {
+        super(s);
+    }
+}
