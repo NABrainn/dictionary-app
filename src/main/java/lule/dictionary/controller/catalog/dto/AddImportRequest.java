dae182d7f4e36ed275d4abd2d6062b4c7a71f2f6
NAbrain <michal2002.60@gmail.com>
2025-06-11 13:30:40 +0200

    add missing content param

--- src/main/java/lule/dictionary/controller/catalog/dto/AddImportRequest.java
+++ src/main/java/lule/dictionary/controller/catalog/dto/AddImportRequest.java
@@ -3,5 +3,10 @@ package lule.dictionary.controller.catalog.dto;
 import lombok.NonNull;
 import lule.dictionary.dto.application.interfaces.userProfile.UserProfileSettings;
 
-public record AddImportRequest(@NonNull String title, @NonNull String url, @NonNull UserProfileSettings userProfileSettings, @NonNull String owner) {
+public record AddImportRequest(
+        @NonNull String title,
+        @NonNull String content,
+        @NonNull String url,
+        @NonNull UserProfileSettings userProfileSettings,
+        @NonNull String owner) {
 }
