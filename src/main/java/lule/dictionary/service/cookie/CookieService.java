3613fad648744c6399dda7fcc4217f3b3895ea2c
NAbrain <michal2002.60@gmail.com>
2025-06-11 13:39:42 +0200

    add cookie service for managing cookies in application

---
+++ src/main/java/lule/dictionary/service/cookie/CookieService.java
@@ -0,0 +1,30 @@
+package lule.dictionary.service.cookie;
+
+import jakarta.servlet.http.Cookie;
+import lombok.NonNull;
+import lule.dictionary.service.jwt.dto.TokenPair;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CookieService {
+
+    public Cookie createJwtCookie(@NonNull String key, @NonNull TokenPair tokenPair) {
+        Cookie tokenCookie = new Cookie(key, tokenPair.accessToken());
+        tokenCookie.setHttpOnly(true);
+        tokenCookie.setPath("/");
+        tokenCookie.setSecure(true);
+        tokenCookie.setMaxAge(24 * 60 * 60);
+        tokenCookie.setAttribute("SameSite", "Strict");
+        return tokenCookie;
+    }
+
+    public Cookie deleteJwtCookie(@NonNull String key) {
+        Cookie tokenCookie = new Cookie(key, null);
+        tokenCookie.setHttpOnly(true);
+        tokenCookie.setPath("/");
+        tokenCookie.setSecure(true);
+        tokenCookie.setMaxAge(0);
+        tokenCookie.setAttribute("SameSite", "Strict");
+        return tokenCookie;
+    }
+}
