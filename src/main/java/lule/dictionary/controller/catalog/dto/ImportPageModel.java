2193d5963da86487e902a6693797a176e4c9ca77
NAbrain <michal2002.60@gmail.com>
2025-06-13 10:28:53 +0200

    change name

--- src/main/java/lule/dictionary/controller/catalog/dto/ImportPageModel.java
+++ src/main/java/lule/dictionary/controller/catalog/dto/ImportPageModel.java
@@ -6,7 +6,7 @@ import lule.dictionary.dto.application.interfaces.translation.Translation;
 import java.util.List;
 import java.util.Map;
 
-public record CatalogModel(@NonNull String title, List<String> content, Map<String, Translation> translations, int importId, int selectedWordId) {
+public record ImportPageModel(@NonNull String title, List<String> content, Map<String, Translation> translations, int importId, int selectedWordId) {
     @Override
     public List<String> content() {
         return List.copyOf(content);
