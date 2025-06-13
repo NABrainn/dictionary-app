785aa01a205d4f2d3012facca2fe1124a1ae8db9
NAbrain <michal2002.60@gmail.com>
2025-06-13 10:22:43 +0200

    pass authentication param to get owner info

--- src/main/java/lule/dictionary/controller/translation/TranslationController.java
+++ src/main/java/lule/dictionary/controller/translation/TranslationController.java
@@ -9,6 +9,7 @@ import lule.dictionary.controller.translation.dto.UpdateFamiliarityRequest;
 import lule.dictionary.enumeration.Familiarity;
 import lule.dictionary.enumeration.Language;
 import lule.dictionary.service.translation.TranslationService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -28,37 +29,36 @@ public class TranslationController {
                                    @RequestParam("selectedWordId") int selectedWordId) {
             boolean found = translationService.findByTargetWord(model, new FindTranslationRequest(importId, targetWord, selectedWordId));
             if(found) {
-                return "translation/update-translation-form";
+                return "import-page/translation/update-translation-form";
             }
-            return "translation/add-translation-form";
+            return "import-page/translation/add-translation-form";
     }
 
     @PostMapping({"/new", "new"})
     public String newTranslation(Model model,
+                                Authentication authentication,
                                 @RequestParam("sourceWord") String sourceWord,
                                 @RequestParam("targetWord") String targetWord,
                                 @RequestParam("familiarity") Familiarity familiarity,
                                 @RequestParam("sourceLanguage") Language sourceLanguage,
                                 @RequestParam("targetLanguage") Language targetLanguage,
-                                @RequestParam("owner") String owner,
                                 @RequestParam("importId") int importId,
                                 @RequestParam("selectedWordId") int selectedWordId) {
-        translationService.add(model, new AddTranslationRequest(sourceWord, targetWord, familiarity, sourceLanguage, targetLanguage, owner, importId, selectedWordId));
+        translationService.add(model, new AddTranslationRequest(sourceWord, targetWord, familiarity, sourceLanguage, targetLanguage, authentication.getName(), importId, selectedWordId));
         return "forward:/catalog/page/reload";
     }
 
     @PutMapping({"/familiarity/update", "familiarity/update"})
     public String updateFamiliarity(Model model,
+                                    Authentication authentication,
                                     @RequestParam("sourceWord") String sourceWord,
                                     @RequestParam("targetWord") String targetWord,
                                     @RequestParam("familiarity") Familiarity familiarity,
                                     @RequestParam("sourceLanguage") Language sourceLanguage,
                                     @RequestParam("targetLanguage") Language targetLanguage,
-                                    @RequestParam("owner") String owner,
                                     @RequestParam("importId") int importId,
                                     @RequestParam("selectedWordId") int selectedWordId) {
-
-        translationService.updateFamiliarity(model, new UpdateFamiliarityRequest(targetWord, familiarity, sourceWord, sourceLanguage, targetLanguage, owner, importId, selectedWordId));
+        translationService.updateFamiliarity(model, new UpdateFamiliarityRequest(targetWord, familiarity, sourceWord, sourceLanguage, targetLanguage, authentication.getName(), importId, selectedWordId));
         return "forward:/catalog/page/reload";
     }
 }
