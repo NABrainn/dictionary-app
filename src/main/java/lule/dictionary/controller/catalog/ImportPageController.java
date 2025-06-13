f048e4a3462b10fa68aa888f6ff737e21581ee64
NAbrain <michal2002.60@gmail.com>
2025-06-13 13:52:08 +0200

    -remove unused APIs -update service name

---
+++ src/main/java/lule/dictionary/controller/catalog/ImportPageController.java
@@ -0,0 +1,42 @@
+package lule.dictionary.controller.catalog;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lule.dictionary.controller.catalog.dto.SaveTranslationRequest;
+import lule.dictionary.service.imports.ImportService;
+import lule.dictionary.service.console.ImportPageService;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@Controller
+@RequestMapping({"/catalog", "/catalog/"})
+@RequiredArgsConstructor
+public class ImportPageController {
+
+    private final ImportService importService;
+    private final ImportPageService importPageService;
+
+    @GetMapping({"", "/"})
+    public String getImportList(Authentication authentication, Model model) {
+        importService.findByOwner(authentication, model, authentication.getName());
+        return "catalog";
+    }
+
+    @PostMapping({"/page/reload", "page/reload"})
+    public String reloadOnPost(Model model,
+                               @RequestParam("selectedWordId") int wordId,
+                               @RequestParam("importId") int importId) {
+        importPageService.loadImportWithTranslations(model, new SaveTranslationRequest(wordId, importId));
+        return "import-page/import-page-content";
+    }
+    @PutMapping({"/page/reload", "page/reload"})
+    public String reloadOnPut(Model model,
+                              @RequestParam("selectedWordId") int wordId,
+                              @RequestParam("importId") int importId) {
+        importPageService.loadImportWithTranslations(model, new SaveTranslationRequest(wordId, importId));
+        return "import-page/import-page-content";
+    }
+}
