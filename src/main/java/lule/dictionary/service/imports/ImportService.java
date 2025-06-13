89e38f180311aba62dd6eed5196ba78c69f9bfd9
NAbrain <michal2002.60@gmail.com>
2025-06-13 10:28:10 +0200

    refactor addImport() api: - handle business logic conditionally depending on whether it has content or url refactor findByOwner() api: - handles adding of model attributes

--- src/main/java/lule/dictionary/service/imports/ImportService.java
+++ src/main/java/lule/dictionary/service/imports/ImportService.java
@@ -3,7 +3,11 @@ package lule.dictionary.service.imports;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lule.dictionary.controller.catalog.dto.AddImportRequest;
+import lule.dictionary.controller.catalog.dto.ImportPageModel;
+import lule.dictionary.controller.catalog.dto.SaveTranslationRequest;
 import lule.dictionary.dto.application.interfaces.imports.Import;
+import lule.dictionary.dto.application.interfaces.translation.Translation;
+import lule.dictionary.enumeration.Language;
 import lule.dictionary.exception.ServiceException;
 import lule.dictionary.factory.dto.ImportFactory;
 import lule.dictionary.repository.ImportRepository;
@@ -12,6 +16,7 @@ import lule.dictionary.service.DocumentParsingService;
 import lule.dictionary.service.auth.validator.exception.ValidationStrategyException;
 import lule.dictionary.service.imports.validator.UrlValidator;
 import org.jsoup.nodes.Document;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
@@ -26,20 +31,32 @@ public class ImportService {
     private final UrlValidator urlValidator;
     private final DocumentParsingService contentParser;
 
-    public int addImport(@NonNull Model model, AddImportRequest addImportRequest) throws ServiceException {
+    public int addImport(Authentication authentication, @NonNull Model model, AddImportRequest addImportRequest) throws ServiceException {
         try {
             if(!addImportRequest.url().startsWith("https://") || !addImportRequest.url().startsWith("http://")) {
                 String urlWithHttps = "https://".concat(addImportRequest.url());
                 try {
-                    urlValidator.validate(urlWithHttps);
-                    Document document = contentParser.fetchContent(urlWithHttps);
-                    String content = document.text();
-                    int importId =  importRepository.addImport(ImportFactory.createImportDetails(
-                            addImportRequest.title(),
-                            content,
-                            addImportRequest.url()
-                    ), addImportRequest.userProfileSettings(), addImportRequest.owner()).orElseThrow(() -> new ServiceException("Failed to add a new import"));
-                    return importId;
+                    if(addImportRequest.content().isEmpty()) {
+                        urlValidator.validate(urlWithHttps);
+                        Document document = contentParser.fetchContent(urlWithHttps);
+                        String content = document.text();
+                        int importId =  importRepository.addImport(ImportFactory.createImportDetails(
+                                addImportRequest.title(),
+                                content,
+                                addImportRequest.url()
+                        ), addImportRequest.userProfileSettings(), addImportRequest.owner()).orElseThrow(() -> new ServiceException("Failed to add a new import"));
+                        model.addAttribute("authentication", authentication);
+                        return importId;
+                    }
+                    else {
+                        int importId =  importRepository.addImport(ImportFactory.createImportDetails(
+                                addImportRequest.title(),
+                                addImportRequest.content(),
+                                addImportRequest.url()
+                        ), addImportRequest.userProfileSettings(), addImportRequest.owner()).orElseThrow(() -> new ServiceException("Failed to add a new import"));
+                        model.addAttribute("authentication", authentication);
+                        return importId;
+                    }
                 } catch (ValidationStrategyException e) {
                     throw new ServiceException(e.getMessage());
                 }
@@ -59,9 +76,11 @@ public class ImportService {
             throw new ServiceException("Failed to fetch import", e.getCause());
         }
     }
-    public List<Import> findByOwner(@NonNull String owner) throws ServiceException {
+    public List<Import> findByOwner(Authentication authentication, @NonNull Model model, @NonNull String owner) throws ServiceException {
         try {
-            return importRepository.findByOwner(owner);
+            List<Import> imports = importRepository.findByOwner(owner);
+            model.addAttribute("imports", imports);
+            return imports;
         } catch (RepositoryException e) {
             throw new ServiceException("Failed to fetch imports", e.getCause());
         }
