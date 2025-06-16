c844d5a74e9e83d702f446b04bae534d9c5e12d0
NAbrain <michal2002.60@gmail.com>
2025-06-16 12:03:12 +0200

    add validation annotations to dtos

--- src/main/java/lule/dictionary/service/auth/dto/LoginRequest.java
+++ src/main/java/lule/dictionary/service/auth/dto/LoginRequest.java
@@ -1,6 +1,17 @@
 package lule.dictionary.service.auth.dto;
 
-import lombok.NonNull;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
-public record LoginRequest(@NonNull String login, @NonNull String password) {
+public record LoginRequest(
+        @NotBlank(message = "Login cannot be empty")
+        @Min(value = 8, message = "Login must be at least 8 characters")
+        @Max(value = 50, message = "Login cannot have more than 50 characters")
+        String login,
+
+        @NotBlank(message = "Password cannot be empty")
+        @Min(value = 8, message = "Password must be at least 8 characters")
+        @Max(value = 200, message = "Password cannot have more than 200 characters")
+        String password) {
 }
--- src/main/java/lule/dictionary/service/auth/dto/SignupRequest.java
+++ src/main/java/lule/dictionary/service/auth/dto/SignupRequest.java
@@ -1,6 +1,24 @@
 package lule.dictionary.service.auth.dto;
 
-import lombok.NonNull;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
-public record SignupRequest(@NonNull String username, @NonNull String email, @NonNull String password) {
+public record SignupRequest(
+        @NotBlank(message = "Login cannot be empty")
+        @Min(value = 8, message = "Login must be at least 8 characters")
+        @Max(value = 50, message = "Login cannot have more than 50 characters")
+        String login,
+
+        @NotBlank(message = "Email cannot be empty")
+        @Min(value = 8, message = "Email must be at least 8 characters")
+        @Max(value = 50, message = "Email cannot have more than 50 characters")
+        @Email
+        String email,
+
+        @NotBlank(message = "Password cannot be empty")
+        @Min(value = 8, message = "Password must be at least 8 characters")
+        @Max(value = 200, message = "Password cannot have more than 200 characters")
+        String password) {
 }
