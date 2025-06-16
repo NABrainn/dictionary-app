51b151e28583704c0a37f4b4169c3b5bc752b47c
NAbrain <michal2002.60@gmail.com>
2025-06-16 12:02:40 +0200

    -apply validation annotations -merge AddTranslationRequest and UpdateTranslationRequest to MutateTranslationRequest to reduce duplication

--- src/main/java/lule/dictionary/service/translation/dto/MutateTranslationRequest.java
+++ src/main/java/lule/dictionary/service/translation/dto/MutateTranslationRequest.java
@@ -1,14 +1,15 @@
 package lule.dictionary.service.translation.dto;
 
+import jakarta.validation.constraints.Max;
 import lombok.Builder;
 import lombok.NonNull;
 import lule.dictionary.enumeration.Familiarity;
 import lule.dictionary.enumeration.Language;
 
 @Builder
-public record AddTranslationRequest(
-        @NonNull String sourceWord,
-        @NonNull String targetWord,
+public record MutateTranslationRequest(
+        @NonNull @Max(value = 50, message = "Source word cannot be longer than 50 characters") String sourceWord,
+        @NonNull @Max(value = 50, message = "Target word cannot be longer than 50 characters") String targetWord,
         @NonNull Familiarity familiarity,
         @NonNull Language sourceLanguage,
         @NonNull Language targetLanguage,
