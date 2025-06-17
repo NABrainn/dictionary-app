848d1428da1f2b1d374b8eebebf70068b7a545e7
NAbrain <michal2002.60@gmail.com>
2025-06-17 13:25:49 +0200

    delete unnecessary field from api

--- src/main/java/lule/dictionary/entity/application/interfaces/imports/ImportDetails.java
+++ src/main/java/lule/dictionary/entity/application/interfaces/imports/ImportDetails.java
@@ -1,10 +1,7 @@
 package lule.dictionary.entity.application.interfaces.imports;
 
-import java.util.List;
-
 public interface ImportDetails {
     String title();
     String content();
     String url();
-    List<String> importDetailsSummary();
 }
