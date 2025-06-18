882471187c07a4d96311466c40377d33152fe2d2
nabrain <michal2002.60@gmail.com>
2025-06-18 12:47:42 +0200

    remove validator pattern due to unnecessary overhead

--- src/main/java/lule/dictionary/service/auth/validator/exception/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package lule.dictionary.service.auth.validator.exception;
-
-public class ValidationException extends RuntimeException{
-    public ValidationException(String message) {
-        super(message);
-    }
-}
--- src/main/java/lule/dictionary/service/auth/validator/exception/ValidationStrategyException.java
+++
@@ -1,7 +0,0 @@
-package lule.dictionary.service.auth.validator.exception;
-
-public class ValidationStrategyException extends RuntimeException{
-    public ValidationStrategyException(String s) {
-        super(s);
-    }
-}
