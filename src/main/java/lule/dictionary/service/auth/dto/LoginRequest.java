31411f00777418a6f2caa3a33a5a98cebba04533
nabrain <michal2002.60@gmail.com>
2025-06-18 12:47:56 +0200

    add pattern validation logic

--- src/main/java/lule/dictionary/service/auth/dto/LoginRequest.java
+++ src/main/java/lule/dictionary/service/auth/dto/LoginRequest.java
@@ -1,13 +1,11 @@
 package lule.dictionary.service.auth.dto;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record LoginRequest(
         @NotBlank(message = "Login cannot be empty")
         @Size(min = 8, max = 50, message = "Login must be between 8 and 50 characters")
+        @Pattern(regexp = "[%&/^!<>@#$'\"*;`:=\\-_+.,(){}\\[\\]?\\\\]", message = "Invalid characters")
         String login,
 
         @NotBlank(message = "Password cannot be empty")
