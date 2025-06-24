75539e02d923e7b21ce01a4909b318bb701b4689
NAbrain <michal2002.60@gmail.com>
2025-06-24 11:33:35 +0200

    adjust ServiceResult calls to updated model

--- src/main/java/lule/dictionary/service/auth/AuthService.java
+++ src/main/java/lule/dictionary/service/auth/AuthService.java
@@ -2,15 +2,16 @@ package lule.dictionary.service.auth;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Validator;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lule.dictionary.service.auth.dto.LoginRequest;
 import lule.dictionary.service.auth.dto.SignupRequest;
 import lule.dictionary.entity.application.interfaces.userProfile.base.UserProfile;
 import lule.dictionary.exception.ResourceNotFoundException;
-import lule.dictionary.exception.ServiceException;
 import lule.dictionary.service.cookie.CookieService;
 import lule.dictionary.service.dto.ServiceResult;
+import lule.dictionary.util.errors.ServiceResultFactory;
 import lule.dictionary.service.jwt.JwtService;
 import lule.dictionary.service.userProfile.UserProfileService;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -23,6 +24,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -34,12 +36,18 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
     private final CookieService cookieService;
+    private final Validator validator;
 
     public void login(@NonNull Model model,
                       @NonNull RedirectAttributes redirectAttributes,
                       @NonNull HttpServletResponse response,
                       @NonNull LoginRequest loginRequest) {
         try {
+            var result = validator.validate(loginRequest);
+            if(!result.isEmpty()) {
+                model.addAttribute("result", new ServiceResult(true, ServiceResultFactory.fromSet(result)));
+                return;
+            }
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
                             loginRequest.login(),
@@ -48,25 +56,27 @@ public class AuthService {
             );
             SecurityContextHolder.getContext().setAuthentication(authentication);
             response.addCookie(cookieService.createJwtCookie("jwt", jwtService.generateTokenPair(authentication)));
-            redirectAttributes.addFlashAttribute("result", new ServiceResult(false, ""));
+            redirectAttributes.addFlashAttribute("result", new ServiceResult(false, Map.of()));
         } catch (AuthenticationException | ResourceNotFoundException e) {
-            model.addAttribute("result", new ServiceResult(true, e.getMessage()));
+            model.addAttribute("result", new ServiceResult(true, Map.of()));
             model.addAttribute("authentication", null);
-            throw new ServiceException(e.getMessage());
         }
     }
 
     public void signup(@NonNull Model model,
                        @NonNull SignupRequest signupRequest) {
+        var result = validator.validate(signupRequest);
+        if(!result.isEmpty()) {
+            model.addAttribute("result", new ServiceResult(true, ServiceResultFactory.fromSet(result)));
+            return;
+        }
         Optional<UserProfile> optionalUserProfile = userProfileService.findByUsernameOrEmail(signupRequest.login(), signupRequest.email());
         if(optionalUserProfile.isPresent()) {
-            String error = "User with given username or email already exists.";
-            model.addAttribute("result", new ServiceResult(true, error));
-            throw new ServiceException(error);
+            model.addAttribute("result", new ServiceResult(true, Map.of("found", "User with given username or email already exists.")));
         }
         String encodedPassword = bCryptPasswordEncoder.encode(signupRequest.password());
         userProfileService.addUserProfile(signupRequest.login(), signupRequest.email(), encodedPassword);
-        model.addAttribute("result", new ServiceResult(false, ""));
+        model.addAttribute("result", new ServiceResult(false, Map.of()));
     }
 
     public void logout(@NonNull RedirectAttributes redirectAttributes,
@@ -75,6 +85,6 @@ public class AuthService {
         SecurityContextHolder.clearContext();
         Cookie cookie = cookieService.deleteJwtCookie("jwt");
         httpServletResponse.addCookie(cookie);
-        redirectAttributes.addFlashAttribute("result", new ServiceResult(false, ""));
+        redirectAttributes.addFlashAttribute("result", new ServiceResult(false, Map.of()));
     }
 }
