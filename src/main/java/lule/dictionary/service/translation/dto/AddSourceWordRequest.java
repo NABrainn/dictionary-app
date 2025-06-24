6fec64e8bcd4c6ed1c1184dda9ade531a3158d74
NAbrain <michal2002.60@gmail.com>
2025-06-24 11:34:14 +0200

    remove redundant aannotations

--- src/main/java/lule/dictionary/service/translation/dto/AddSourceWordRequest.java
+++ src/main/java/lule/dictionary/service/translation/dto/AddSourceWordRequest.java
@@ -1,6 +1,6 @@
 package lule.dictionary.service.translation.dto;
 
-import jakarta.validation.constraints.NotBlank;
-
-public record AddSourceWordRequest(@NotBlank(message = "source word cannot be blank") String sourceWord, int importId, @NotBlank(message = "target word cannot be blank") String targetWord, int selectedWordId) {
+public record AddSourceWordRequest(
+        String sourceWord, int importId,
+        String targetWord, int selectedWordId) {
 }
