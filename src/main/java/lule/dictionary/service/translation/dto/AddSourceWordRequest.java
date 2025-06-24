8a7f550d2bdbcdeea9d175d8387ee57238c5e0d9
NAbrain <michal2002.60@gmail.com>
2025-06-24 14:49:35 +0200

    delete

--- src/main/java/lule/dictionary/service/translation/dto/AddSourceWordRequest.java
+++
@@ -1,6 +0,0 @@
-package lule.dictionary.service.translation.dto;
-
-public record AddSourceWordRequest(
-        String sourceWord, int importId,
-        String targetWord, int selectedWordId) {
-}
