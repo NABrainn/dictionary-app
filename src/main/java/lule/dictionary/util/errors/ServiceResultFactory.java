22fd820f38666e24eaf9068c7ddf56adf7bbe2ef
NAbrain <michal2002.60@gmail.com>
2025-06-24 11:35:14 +0200

    filter out nulls

--- src/main/java/lule/dictionary/util/errors/ServiceResultFactory.java
+++ src/main/java/lule/dictionary/util/errors/ServiceResultFactory.java
@@ -3,6 +3,7 @@ package lule.dictionary.util.errors;
 import jakarta.validation.ConstraintViolation;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -10,6 +11,7 @@ public class ServiceResultFactory {
 
     public static <T> Map<String, String> fromSet(Set<ConstraintViolation<T>> result) {
         return result.stream()
+                .filter(Objects::nonNull)
                 .collect(Collectors.toUnmodifiableMap(
                         ConstraintViolation::getMessage,
                         v -> v.getPropertyPath().toString()));
