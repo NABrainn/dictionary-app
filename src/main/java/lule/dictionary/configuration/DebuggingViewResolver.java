c49fd3b4ca03f190e906c6a7ea0b7093673a4813
NAbrain <michal2002.60@gmail.com>
2025-06-30 19:58:34 +0200

    d

---
+++ src/main/java/lule/dictionary/configuration/DebuggingViewResolver.java
@@ -0,0 +1,23 @@
+package lule.dictionary.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+
+import java.util.List;
+
+@Configuration
+public class DebuggingViewResolver {
+    @Bean
+    public ViewResolver debuggingViewResolver(List<ViewResolver> resolvers) {
+        return (viewName, locale) -> {
+            System.out.println(">>> Resolving view: " + viewName);
+            for (ViewResolver r : resolvers) {
+                var view = r.resolveViewName(viewName, locale);
+                System.out.println(" - " + r.getClass().getSimpleName() + " -> " + (view != null ? view : "null"));
+            }
+            return null;
+        };
+    }
+}
+
