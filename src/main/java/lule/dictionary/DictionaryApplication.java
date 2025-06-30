acca0100eac436cf8626b1888bc0de698910b077
NAbrain <michal2002.60@gmail.com>
2025-06-30 14:23:44 +0200

    add component scan

--- src/main/java/lule/dictionary/DictionaryApplication.java
+++ src/main/java/lule/dictionary/DictionaryApplication.java
@@ -2,8 +2,10 @@ package lule.dictionary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "lule.dictionary")
 public class DictionaryApplication {
 	public static void main(String... args) throws Exception {
 		SpringApplication.run(DictionaryApplication.class, args);
