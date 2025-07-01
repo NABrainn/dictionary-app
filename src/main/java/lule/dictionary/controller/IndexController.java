7a446cf419c5ebc741b4e7f0643ccc255834725d
NAbrain <michal2002.60@gmail.com>
2025-07-01 10:24:37 +0200

    set to ""

--- src/main/java/lule/dictionary/controller/IndexController.java
+++ src/main/java/lule/dictionary/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("`")
+@RequestMapping("")
 public class IndexController {
 
     @GetMapping("/")
