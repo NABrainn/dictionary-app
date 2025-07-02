3e3aaa33bc5efa2758cf79b295c09fabfd23ae5b
NAbrain <michal2002.60@gmail.com>
2025-07-02 13:05:34 +0200

    refactor queries to account for new streaks table

--- src/main/java/lule/dictionary/repository/UserProfileRepository.java
+++ src/main/java/lule/dictionary/repository/UserProfileRepository.java
@@ -22,10 +22,11 @@ public class UserProfileRepository {
 
     public Optional<UserProfile> findByUsername(@NonNull String username) {
         String sql = """
-                SELECT p.username, p.password, p.email, s.source_lang, s.target_lang
-                FROM dictionary.user_profiles p
-                LEFT JOIN dictionary.user_profile_settings s ON p.settings_id=s.settings_id
-                WHERE p.username=?
+                    SELECT p.username, p.password, p.email, s.source_lang, s.target_lang, str.day_count, str.is_daily_goal_met, str.updated_at
+                    FROM dictionary.user_profiles p
+                    LEFT JOIN dictionary.user_profile_settings s ON p.settings_id=s.settings_id
+                    LEFT JOIN dictionary.streaks str ON p.username=str.streak_owner
+                    WHERE p.username=?;
                 """;
         try {
             List<UserProfile> result = template.query(sql, USER_PROFILE, username);
@@ -38,10 +39,11 @@ public class UserProfileRepository {
 
     public Optional<UserProfile> findByUsernameOrEmail(@NonNull String username, @NonNull String email) {
         String sql = """
-                SELECT p.username, p.password, p.email, s.source_lang, s.target_lang
-                FROM dictionary.user_profiles p
-                LEFT JOIN dictionary.user_profile_settings s ON p.settings_id=s.settings_id
-                WHERE p.username=? OR p.email=?
+                    SELECT p.username, p.password, p.email, s.source_lang, s.target_lang, str.day_count, str.is_daily_goal_met, str.updated_at
+                    FROM dictionary.user_profiles p
+                    LEFT JOIN dictionary.user_profile_settings s ON p.settings_id=s.settings_id
+                    LEFT JOIN dictionary.streaks str ON p.username=str.streak_owner
+                    WHERE p.username=? OR p.email=?;
                 """;
         try {
             List<UserProfile> result = template.query(sql, USER_PROFILE, username, email);
@@ -59,15 +61,21 @@ public class UserProfileRepository {
                         VALUES (?, ?)
                         RETURNING settings_id, source_lang, target_lang
                     ),
+                    streak AS (
+                        INSERT INTO dictionary.streaks (day_count, is_daily_goal_met, streak_owner, updated_at)
+                        VALUES (0, false, 'useruseruser', '2025-07-02 15:00:00+02')
+                        RETURNING day_count, is_daily_goal_met, streak_owner, updated_at
+                    ),
                     user_insert AS (
                         INSERT INTO dictionary.user_profiles (username, email, password, settings_id)
                         SELECT ?, ?, ?, s.settings_id
                         FROM settings s
                         RETURNING username, email, password, settings_id
                     )
-                    SELECT u.username, u.email, u.password, s.source_lang, s.target_lang
+                    SELECT u.username, u.email, u.password, s.source_lang, s.target_lang, str.day_count, str.is_daily_goal_met, str.updated_at
                     FROM user_insert u
-                    JOIN settings s ON u.settings_id = s.settings_id;
+                    LEFT JOIN settings s ON u.settings_id = s.settings_id
+                    LEFT JOIN streak str ON u.username = str.streak_owner;
                 """;
         try {
             List<UserProfile> addedUser = template.query(sql, USER_PROFILE,
@@ -86,10 +94,10 @@ public class UserProfileRepository {
 
     public List<UserProfile> findAll() {
         String sql = """
-                SELECT p.username, p.email, p.password, s.source_lang, s.target_lang
-                FROM dictionary.user_profiles as p
-                LEFT JOIN dictionary.user_profile_settings as s
-                ON p.settings_id=s.settings_id
+                    SELECT p.username, p.email, p.password, s.source_lang, s.target_lang, str.day_count, str.is_daily_goal_met, str.updated_at
+                    FROM dictionary.user_profiles as p
+                    LEFT JOIN dictionary.user_profile_settings s ON p.settings_id=s.settings_id
+                    LEFT JOIN dictionary.streaks str ON p.username=str.streak_owner
                 """;
         try {
             return template.query(sql, USER_PROFILE);
