6bf67d4f3a912dc50a4abf13e7bcb43f25d1fdc7
NAbrain <michal2002.60@gmail.com>
2025-07-02 13:23:12 +0200

    add timezone param

--- src/main/java/lule/dictionary/repository/UserProfileRepository.java
+++ src/main/java/lule/dictionary/repository/UserProfileRepository.java
@@ -22,7 +22,7 @@ public class UserProfileRepository {
 
     public Optional<UserProfile> findByUsername(@NonNull String username) {
         String sql = """
-                    SELECT p.username, p.password, p.email, s.source_lang, s.target_lang, str.day_count, str.is_daily_goal_met, str.updated_at
+                    SELECT p.username, p.password, p.email, s.source_lang, s.target_lang, str.day_count, str.is_daily_goal_met, str.timezone, str.updated_at
                     FROM dictionary.user_profiles p
                     LEFT JOIN dictionary.user_profile_settings s ON p.settings_id=s.settings_id
                     LEFT JOIN dictionary.streaks str ON p.username=str.streak_owner
@@ -39,7 +39,7 @@ public class UserProfileRepository {
 
     public Optional<UserProfile> findByUsernameOrEmail(@NonNull String username, @NonNull String email) {
         String sql = """
-                    SELECT p.username, p.password, p.email, s.source_lang, s.target_lang, str.day_count, str.is_daily_goal_met, str.updated_at
+                    SELECT p.username, p.password, p.email, s.source_lang, s.target_lang, str.day_count, str.is_daily_goal_met, str.timezone, str.updated_at
                     FROM dictionary.user_profiles p
                     LEFT JOIN dictionary.user_profile_settings s ON p.settings_id=s.settings_id
                     LEFT JOIN dictionary.streaks str ON p.username=str.streak_owner
@@ -62,8 +62,8 @@ public class UserProfileRepository {
                         RETURNING settings_id, source_lang, target_lang
                     ),
                     streak AS (
-                        INSERT INTO dictionary.streaks (day_count, is_daily_goal_met, streak_owner, updated_at)
-                        VALUES (0, false, 'useruseruser', '2025-07-02 15:00:00+02')
+                        INSERT INTO dictionary.streaks (day_count, is_daily_goal_met, streak_owner, timezone, updated_at)
+                        VALUES (0, false, 'useruseruser', ?, ?)
                         RETURNING day_count, is_daily_goal_met, streak_owner, updated_at
                     ),
                     user_insert AS (
@@ -72,7 +72,7 @@ public class UserProfileRepository {
                         FROM settings s
                         RETURNING username, email, password, settings_id
                     )
-                    SELECT u.username, u.email, u.password, s.source_lang, s.target_lang, str.day_count, str.is_daily_goal_met, str.updated_at
+                    SELECT u.username, u.email, u.password, s.source_lang, s.target_lang, str.day_count, str.is_daily_goal_met, str.timezone, str.updated_at
                     FROM user_insert u
                     LEFT JOIN settings s ON u.settings_id = s.settings_id
                     LEFT JOIN streak str ON u.username = str.streak_owner;
@@ -81,6 +81,8 @@ public class UserProfileRepository {
             List<UserProfile> addedUser = template.query(sql, USER_PROFILE,
                     userProfile.sourceLanguage().name(),
                     userProfile.targetLanguage().name(),
+                    //timezone
+                    //updatedAt
                     userProfile.username(),
                     userProfile.email(),
                     userProfile.password()
@@ -94,7 +96,7 @@ public class UserProfileRepository {
 
     public List<UserProfile> findAll() {
         String sql = """
-                    SELECT p.username, p.email, p.password, s.source_lang, s.target_lang, str.day_count, str.is_daily_goal_met, str.updated_at
+                    SELECT p.username, p.email, p.password, s.source_lang, s.target_lang, str.day_count, str.is_daily_goal_met, str.timezone, str.updated_at
                     FROM dictionary.user_profiles as p
                     LEFT JOIN dictionary.user_profile_settings s ON p.settings_id=s.settings_id
                     LEFT JOIN dictionary.streaks str ON p.username=str.streak_owner
