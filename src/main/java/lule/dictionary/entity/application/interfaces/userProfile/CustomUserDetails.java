512ff8000aa4fb9308afca86cdd37d7d4414519a
NAbrain <michal2002.60@gmail.com>
2025-07-03 12:30:52 +0200

    add offset

--- src/main/java/lule/dictionary/entity/application/interfaces/userProfile/CustomUserDetails.java
+++ src/main/java/lule/dictionary/entity/application/interfaces/userProfile/CustomUserDetails.java
@@ -4,6 +4,7 @@ import lule.dictionary.service.language.Language;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface CustomUserDetails extends UserDetails {
+    String offset();
     Language sourceLanguage();
     Language targetLanguage();
 }
