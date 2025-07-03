5e0de724b8a4e5fbcde82a3180c2043e36f06aed
NAbrain <michal2002.60@gmail.com>
2025-07-03 12:24:17 +0200

    utils for managing timezone info

---
+++ src/main/java/lule/dictionary/configuration/security/filter/timezone/TimeZoneOffsetContext.java
@@ -0,0 +1,19 @@
+package lule.dictionary.configuration.security.filter.timezone;
+
+import lombok.Data;
+
+public class TimeZoneOffsetContext {
+    private static final ThreadLocal<String> timeZoneOffsetHolder = new ThreadLocal<>();
+
+    public static String get() {
+        return timeZoneOffsetHolder.get();
+    }
+
+    public static void set(String timeZoneOffset) {
+        timeZoneOffsetHolder.set(timeZoneOffset);
+    }
+
+    public static void reset() {
+        timeZoneOffsetHolder.remove();
+    }
+}
