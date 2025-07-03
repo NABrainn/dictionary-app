d253b40c5e28888cf8398bbac3e5af73134d5ac8
NAbrain <michal2002.60@gmail.com>
2025-07-03 14:33:30 +0200

    refactor to use timezone id

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -25,7 +25,8 @@ CREATE TABLE IF NOT EXISTS dictionary.streaks (
 	day_count						int NOT NULL DEFAULT 0,
 	words_added_today				int	NOT NULL DEFAULT 0,
 	streak_owner					VARCHAR(50) REFERENCES dictionary.user_profiles(username) ON UPDATE CASCADE ON DELETE CASCADE,
-	tz_offset                        VARCHAR(10) NOT NULL DEFAULT 'CET',
+	tz_offset                       VARCHAR(100) NOT NULL DEFAULT 'CET',
+	last_day_counted                DATE;
 	updated_at						TIMESTAMPTZ
 );
 
