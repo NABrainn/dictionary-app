19a19d63849da9576e3ae68413841dce439fcfb2
NAbrain <michal2002.60@gmail.com>
2025-07-04 18:01:45 +0200

    add new api to handle wildcard type constraints

--- src/main/java/lule/dictionary/util/errors/ErrorMapFactory.java
+++ src/main/java/lule/dictionary/util/errors/ErrorMapFactory.java
@@ -8,7 +8,19 @@ import java.util.stream.Collectors;
 
 public class ErrorMapFactory {
 
-    public static <T> Map<String, String> fromSet(Set<ConstraintViolation<T>> result) {
+    public static <T> Map<String, String> fromSetTyped(Set<ConstraintViolation<T>> result) {
+        Map<String, String> sorted = result.stream()
+                .filter(Objects::nonNull)
+                .collect(Collectors.toMap(
+                        v -> v.getPropertyPath().toString(),
+                        ConstraintViolation::getMessage,
+                        (existing, ignored) -> existing,
+                        TreeMap::new
+                ));
+        return Collections.unmodifiableMap(sorted);
+    }
+
+    public static <T> Map<String, String> fromSetWildcard(Set<ConstraintViolation<?>> result) {
         Map<String, String> sorted = result.stream()
                 .filter(Objects::nonNull)
                 .collect(Collectors.toMap(
