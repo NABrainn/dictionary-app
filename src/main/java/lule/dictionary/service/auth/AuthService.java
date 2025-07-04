46acdd3e09cdd5d472ec4235e7ae811630a18320
NAbrain <michal2002.60@gmail.com>
2025-07-04 14:37:32 +0200

    extract public api code into methods

--- src/main/java/lule/dictionary/service/auth/AuthService.java
+++ src/main/java/lule/dictionary/service/auth/AuthService.java
@@ -2,16 +2,19 @@ package lule.dictionary.service.auth;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lule.dictionary.configuration.security.filter.timezone.TimeZoneOffsetContext;
 import lule.dictionary.exception.RetryViewException;
+import lule.dictionary.service.auth.dto.AuthRequest;
 import lule.dictionary.service.auth.dto.LoginRequest;
 import lule.dictionary.service.auth.dto.SignupRequest;
 import lule.dictionary.entity.application.interfaces.userProfile.base.UserProfile;
 import lule.dictionary.service.cookie.CookieService;
 import lule.dictionary.service.dto.ServiceResult;
+import lule.dictionary.service.dto.ServiceResultFactory;
 import lule.dictionary.service.userProfile.exception.UserExistsException;
 import lule.dictionary.service.userProfile.exception.UserNotFoundException;
 import lule.dictionary.util.errors.ErrorMapFactory;
@@ -30,6 +33,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -42,40 +46,27 @@ public class AuthService {
     private final CookieService cookieService;
     private final Validator validator;
 
-    public void login(@NonNull Model model,
+    public void login(@NonNull Model clientModel,
                       @NonNull RedirectAttributes redirectAttributes,
                       @NonNull HttpServletResponse response,
                       @NonNull LoginRequest loginRequest) {
         try {
-            var constraints = validator.validate(loginRequest);
+            var constraints = getConstraintViolations(loginRequest);
             if(!constraints.isEmpty()) {
-                var result = new ServiceResult(true, ErrorMapFactory.fromSet(constraints));
-                model.addAttribute("result", result);
-                throw new RetryViewException("validation failure");
+                sendResultAndRetry(clientModel, ServiceResultFactory.createErrorResult(ErrorMapFactory.fromSet(constraints)));
             }
-            UserProfile user = userProfileService.findByUsername(loginRequest.login());
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            user.username(),
-                            loginRequest.password()
-                    )
-            );
-            TimeZoneOffsetContext.set(user.offset());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            response.addCookie(cookieService.createJwtCookie("jwt", jwtService.generateTokenPair(authentication)));
-            var result = new ServiceResult(false, Map.of());
-            redirectAttributes.addFlashAttribute("result", result);
+            UserProfile user = getUserProfile(loginRequest);
+            Authentication authentication = getAuthentication(loginRequest, user);
+            setAuthentication(authentication);
+            setTimezoneOffset(user);
+            sendJwtCookie(response, authentication);
+            sendResult(redirectAttributes, ServiceResultFactory.createSuccessResult(Map.of()));
 
         } catch (AuthenticationException e) {
-            var result = new ServiceResult(false, Map.of());
-            model.addAttribute("result", result);
-            throw new RetryViewException("Authentication exception");
+            sendResultAndRetry(clientModel, ServiceResultFactory.createErrorResult(Map.of("auth", "Authentication failed")));
 
         } catch (UserNotFoundException e) {
-            var result = new ServiceResult(true, Map.of("login", "User does not exist"));
-            model.addAttribute("result", result);
-            model.addAttribute("authentication", null);
-            throw new RetryViewException("User does not exist");
+            sendResultAndRetry(clientModel, ServiceResultFactory.createErrorResult(Map.of("login", "User does not exist")));
         }
     }
 
@@ -83,42 +74,102 @@ public class AuthService {
     public void signup(@NonNull Model model,
                        @NonNull String timeZone,
                        @NonNull SignupRequest signupRequest) {
-        var constraints = validator.validate(signupRequest);
-
+        var constraints = getConstraintViolations(signupRequest);
         if(!constraints.isEmpty()) {
-            var result = new ServiceResult(true, ErrorMapFactory.fromSet(constraints));
-            model.addAttribute("result", result);
-            throw new RetryViewException("validation failure");
+            sendResultAndRetry(model, ServiceResultFactory.createErrorResult(ErrorMapFactory.fromSet(constraints)));
         }
-
-        Optional<UserProfile> optionalUserProfile = userProfileService.findByUsernameOrEmail(signupRequest.login(), signupRequest.email());
+        Optional<UserProfile> optionalUserProfile = getUserProfile(signupRequest);
         if(optionalUserProfile.isPresent()) {
-            var result = new ServiceResult(true, Map.of("login", "User with given username or email already exists."));
-            model.addAttribute("result", result);
-            throw new RetryViewException("User with given username already exists.");
+            sendResultAndRetry(model, ServiceResultFactory.createErrorResult(Map.of("login", "User with given username or email already exists.")));
         }
 
-        String encodedPassword = bCryptPasswordEncoder.encode(signupRequest.password());
-
+        String encodedPassword = getEncodedPassword(signupRequest);
         try {
             userProfileService.addUserProfile(signupRequest.login(), signupRequest.email(), encodedPassword, timeZone);
         } catch (UserExistsException e) {
-            var result = new ServiceResult(true, Map.of());
-            model.addAttribute("result", result);
-            throw new RetryViewException(e.getMessage());
+            sendResultAndRetry(model, ServiceResultFactory.createErrorResult(Map.of()));
         }
-
-        var result = new ServiceResult(false, Map.of());
-        model.addAttribute("result", result);
-
+        sendResult(model, ServiceResultFactory.createSuccessResult(Map.of()));
     }
 
     public void logout(@NonNull RedirectAttributes redirectAttributes,
                        @NonNull HttpServletResponse httpServletResponse) {
-        SecurityContextHolder.getContext().setAuthentication(null);
+        clearAuthentication();
+        deleteJwtCookie(httpServletResponse);
+        sendResult(redirectAttributes, ServiceResultFactory.createSuccessResult(Map.of()));
+    }
+
+    private void clearAuthentication() {
+        setAuthentication(null);
         SecurityContextHolder.clearContext();
+    }
+
+    private String getEncodedPassword(SignupRequest signupRequest) {
+        String encodedPassword = bCryptPasswordEncoder.encode(signupRequest.password());
+        return encodedPassword;
+    }
+
+    private Optional<UserProfile> getUserProfile(SignupRequest signupRequest) {
+        Optional<UserProfile> optionalUserProfile = userProfileService.findByUsernameOrEmail(signupRequest.login(), signupRequest.email());
+        return optionalUserProfile;
+    }
+
+    private void sendResultAndRetry(Model model, ServiceResult result) throws RetryViewException {
+        sendResult(model, result);
+        String exceptionMessage = readMessageFromResult(result);
+        throw new RetryViewException(exceptionMessage);
+    }
+
+    private void sendResult(Model clientModel, ServiceResult serviceResult) {
+        clientModel.addAttribute("result", serviceResult);
+    }
+
+    private String readMessageFromResult(ServiceResult result) throws RuntimeException {
+        return result.errors().values().stream()
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("ServiceResult is lacking message values"));
+    }
+
+    private void deleteJwtCookie(HttpServletResponse response) {
         Cookie cookie = cookieService.deleteJwtCookie("jwt");
-        httpServletResponse.addCookie(cookie);
-        redirectAttributes.addFlashAttribute("result", new ServiceResult(false, Map.of()));
+        response.addCookie(cookie);
+    }
+
+    private Set<ConstraintViolation<@NonNull AuthRequest>> getConstraintViolations(AuthRequest authRequest) {
+        return validator.validate(authRequest);
+    }
+
+
+    private void sendJwtCookie(HttpServletResponse response, Authentication authentication) {
+        Cookie jwtCookie = createJwtCookie(authentication);
+        response.addCookie(jwtCookie);
+    }
+
+    private Cookie createJwtCookie(Authentication authentication) {
+        Cookie jwtCookie = cookieService.createJwtCookie("jwt", jwtService.generateTokenPair(authentication));
+        return jwtCookie;
+    }
+
+    private void setAuthentication(Authentication authentication) {
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private void setTimezoneOffset(UserProfile user) {
+        TimeZoneOffsetContext.set(user.offset());
+    }
+
+    private Authentication getAuthentication(LoginRequest loginRequest, UserProfile user) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        user.username(),
+                        loginRequest.password()
+                )
+        );
+        return authentication;
+    }
+
+    private UserProfile getUserProfile(LoginRequest loginRequest) {
+        UserProfile user = userProfileService.findByUsername(loginRequest.login());
+        return user;
     }
 }
