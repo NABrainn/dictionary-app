42452be2e4bc0f6758f2e9f81a5fdc2e762b93f8
nabrain <michal2002.60@gmail.com>
2025-07-10 13:01:02 +0200

    update TranslationService api

--- src/main/java/lule/dictionary/service/translation/TranslationService.java
+++ src/main/java/lule/dictionary/service/translation/TranslationService.java
@@ -3,9 +3,11 @@ package lule.dictionary.service.translation;
 import lule.dictionary.service.dto.result.ServiceResult;
 import lule.dictionary.service.translation.dto.FindByTargetWordRequest;
 import lule.dictionary.service.translation.dto.TranslationAttribute;
+import lule.dictionary.service.translation.dto.UpdateTranslationFamiliarityRequest;
 import lule.dictionary.service.translation.dto.request.AddTranslationRequest;
 
 public interface TranslationService {
     ServiceResult<TranslationAttribute> createTranslation(AddTranslationRequest request);
     ServiceResult<TranslationAttribute> findByTargetWord(FindByTargetWordRequest request);
+    ServiceResult<TranslationAttribute> updateFamiliarity(UpdateTranslationFamiliarityRequest request);
 }
