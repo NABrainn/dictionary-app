2b35dfe204de3f8b2c60befe9bcbb3302a75409f
nabrain <michal2002.60@gmail.com>
2025-07-10 13:38:09 +0200

    update TranslationService api

--- src/main/java/lule/dictionary/service/translation/TranslationService.java
+++ src/main/java/lule/dictionary/service/translation/TranslationService.java
@@ -1,13 +1,18 @@
 package lule.dictionary.service.translation;
 
 import lule.dictionary.service.dto.result.ServiceResult;
-import lule.dictionary.service.translation.dto.FindByTargetWordRequest;
-import lule.dictionary.service.translation.dto.TranslationAttribute;
-import lule.dictionary.service.translation.dto.UpdateTranslationFamiliarityRequest;
+import lule.dictionary.service.translation.dto.DeleteSourceWordRequest;
+import lule.dictionary.service.translation.dto.attribute.TranslationAttribute;
+import lule.dictionary.service.translation.dto.attribute.TranslationPair;
 import lule.dictionary.service.translation.dto.request.AddTranslationRequest;
+import lule.dictionary.service.translation.dto.request.FindByTargetWordRequest;
+import lule.dictionary.service.translation.dto.request.UpdateSourceWordsRequest;
+import lule.dictionary.service.translation.dto.request.UpdateTranslationFamiliarityRequest;
 
 public interface TranslationService {
     ServiceResult<TranslationAttribute> createTranslation(AddTranslationRequest request);
     ServiceResult<TranslationAttribute> findByTargetWord(FindByTargetWordRequest request);
     ServiceResult<TranslationAttribute> updateFamiliarity(UpdateTranslationFamiliarityRequest request);
+    ServiceResult<TranslationPair> updateSourceWords(UpdateSourceWordsRequest request);
+    ServiceResult<TranslationPair> deleteSourceWord(DeleteSourceWordRequest request);
 }
