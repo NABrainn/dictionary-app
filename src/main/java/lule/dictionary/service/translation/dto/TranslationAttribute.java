8d38211b78402e9f69ca7f251e117679da91ef53
nabrain <michal2002.60@gmail.com>
2025-07-10 13:01:35 +0200

    initialize uninitialized translationId with -1

--- src/main/java/lule/dictionary/service/translation/dto/TranslationAttribute.java
+++ src/main/java/lule/dictionary/service/translation/dto/TranslationAttribute.java
@@ -16,6 +16,16 @@ public record TranslationAttribute(int importId,
                                    int currentFamiliarity,
                                    @NonNull Map<Integer, Familiarity> familiarityLevels,
                                    int page) {
+    public static final int UNINITIALIZED_ID = -1;
+
+    public TranslationAttribute {
+        if (translationId == 0) {
+            translationId = UNINITIALIZED_ID;
+        }
+
+        familiarityLevels = Collections.unmodifiableMap(familiarityLevels);
+    }
+
     @Override
     public Map<Integer, Familiarity> familiarityLevels() {
         return Collections.unmodifiableMap(familiarityLevels);
