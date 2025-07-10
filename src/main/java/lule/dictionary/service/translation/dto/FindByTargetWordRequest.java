6310262d544f77030988e7459c5375453db7575e
nabrain <michal2002.60@gmail.com>
2025-07-10 12:48:55 +0200

    add FindByTargetWordRequest

---
+++ src/main/java/lule/dictionary/service/translation/dto/FindByTargetWordRequest.java
@@ -0,0 +1,21 @@
+package lule.dictionary.service.translation.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lule.dictionary.service.dto.request.ServiceRequest;
+import lule.dictionary.service.language.Language;
+
+@Builder
+public record FindByTargetWordRequest(int importId,
+                                      @NotBlank(message = "Target word cannot be blank")
+                                      @Size(max = 200, message = "Target word cannot be longer than 200 characters")
+                                      @Pattern(regexp = "^[\\p{L}0-9 ]+$", message = "Source word contains invalid characters")
+                                      int selectedWordId,
+                                      String targetWord,
+                                      Language sourceLanguage,
+                                      Language targetLanguage,
+                                      int page,
+                                      String owner) implements ServiceRequest {
+}
