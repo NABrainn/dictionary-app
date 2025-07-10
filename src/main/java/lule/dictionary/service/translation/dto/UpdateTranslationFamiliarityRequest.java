3e4365c903a23c7527cd05dee1a598d9fb21677e
nabrain <michal2002.60@gmail.com>
2025-07-10 12:58:36 +0200

    -implements ServiceRequest -add @Builder annotation

--- src/main/java/lule/dictionary/service/translation/dto/UpdateTranslationFamiliarityRequest.java
+++ src/main/java/lule/dictionary/service/translation/dto/UpdateTranslationFamiliarityRequest.java
@@ -3,19 +3,22 @@ package lule.dictionary.service.translation.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.NonNull;
 import lule.dictionary.enumeration.Familiarity;
+import lule.dictionary.service.dto.request.ServiceRequest;
 import lule.dictionary.service.language.Language;
 
+@Builder
 public record UpdateTranslationFamiliarityRequest(@NotBlank
-                                                @Size(max = 200, message = "Target word cannot be longer than 200 characters")
-                                                @Pattern(regexp = "^[\\p{L}0-9 ]+$", message = "Source word contains invalid characters")
-                                                String targetWord,
-                                                @NonNull Familiarity familiarity,
-                                                @NonNull Language sourceLanguage,
-                                                @NonNull Language targetLanguage,
-                                                @NonNull String owner,
-                                                int importId,
-                                                int selectedWordId,
-                                                int page) {
+                                                  @Size(max = 200, message = "Target word cannot be longer than 200 characters")
+                                                  @Pattern(regexp = "^[\\p{L}0-9 ]+$", message = "Source word contains invalid characters")
+                                                  String targetWord,
+                                                  @NonNull Familiarity familiarity,
+                                                  @NonNull Language sourceLanguage,
+                                                  @NonNull Language targetLanguage,
+                                                  @NonNull String owner,
+                                                  int importId,
+                                                  int selectedWordId,
+                                                  int page) implements ServiceRequest {
 }
