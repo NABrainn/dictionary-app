c66b6bb460161d32c35d088fc354d503e1e6f022
nabrain <michal2002.60@gmail.com>
2025-07-13 13:50:35 +0200

    refactor LanguageHelper: -holds data of all languages -holds apis for getting concrete language params from concrete Language

--- src/main/java/lule/dictionary/service/language/LanguageHelper.java
+++ src/main/java/lule/dictionary/service/language/LanguageHelper.java
@@ -1,20 +1,47 @@
 package lule.dictionary.service.language;
 
+import lule.dictionary.dto.application.LanguageData;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+@Service
 public class LanguageHelper {
 
-    public static Map<Language, String> languageMapNames = Map.of(
-            Language.NO, "Norwegian",
-            Language.PL, "Polish",
-            Language.EN, "English",
-            Language.IT, "Italian"
+    private final List<LanguageData> allLanguageData = List.of(
+            LanguageData.of(Language.NO, "Norwegian", "nb"),
+            LanguageData.of(Language.PL, "Polish", "pl"),
+            LanguageData.of(Language.EN, "English", "en"),
+            LanguageData.of(Language.IT, "Italian", "it")
     );
 
-    public static Map<Language, String> languageMapAbbreviations = Map.of(
-            Language.PL, "pl",
-            Language.EN, "en",
-            Language.NO, "nb",
-            Language.IT, "it"
-    );
+    public String getFullName(Language language) {
+        return languageFullNames().get(language);
+    }
+
+    public String getAbbreviation(Language language) {
+        return languageAbbreviations().get(language);
+    }
+
+    public List<LanguageData> getAllLanguageData() {
+        return List.copyOf(allLanguageData);
+    }
+
+    private Map<Language, String> languageFullNames() {
+        Map<Language, String> map = new HashMap<>();
+        for (LanguageData languageData : allLanguageData) {
+            map.put(languageData.language(), languageData.fullName());
+        }
+        return map;
+    }
+
+    private Map<Language, String> languageAbbreviations() {
+        Map<Language, String> map = new HashMap<>();
+        for (LanguageData languageData : allLanguageData) {
+            map.put(languageData.language(), languageData.libreTranslateAbbreviation());
+        }
+        return map;
+    }
 }
