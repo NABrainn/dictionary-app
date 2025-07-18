965ccab4aff3899609ebf23f6ed1e61ac316bc57
nabrain <michal2002.60@gmail.com>
2025-07-18 13:20:05 +0200

    update jte template semantics and structure

--- src/main/java/lule/dictionary/controller/ollama/OllamaController.java
+++ src/main/java/lule/dictionary/controller/ollama/OllamaController.java
@@ -28,13 +28,13 @@ public class OllamaController {
         String sourceWord = ollamaService.translate(principal.targetLanguage(), principal.sourceLanguage(), targetWord);
         model.addAttribute("translation", sourceWord);
         model.addAttribute("targetWord", targetWord);
-        return "document-page/translation/ai-translation";
+        return "document-page/content/translation/ai-translation";
     }
 
     @GetMapping({"/closeAiTranslation", "/closeAiTranslation/"})
     public String closeAiTranslation(@RequestParam(name = "targetWord") String targetWord,
                                      Model model) {
         model.addAttribute("targetWord", targetWord);
-        return "document-page/translation/get-ai-translation-btn";
+        return "document-page/content/translation/get-ai-translation-btn";
     }
 }
