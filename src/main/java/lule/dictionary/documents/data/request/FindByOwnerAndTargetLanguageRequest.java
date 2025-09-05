6f90bb3772114cf8d917bdbece3582ca6baea22b
nabrain <michal2002.60@gmail.com>
2025-09-05 15:17:20 +0200

    delete

--- src/main/java/lule/dictionary/documents/data/request/FindByOwnerAndTargetLanguageRequest.java
+++
@@ -1,9 +0,0 @@
-package lule.dictionary.documents.data.request;
-
-import lule.dictionary.language.service.Language;
-
-public record FindByOwnerAndTargetLanguageRequest(String owner, Language targetLanguage) {
-    public static FindByOwnerAndTargetLanguageRequest of(String username, Language language) {
-        return new FindByOwnerAndTargetLanguageRequest(username, language);
-    }
-}
