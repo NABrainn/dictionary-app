83b61b7dcc647f973f7c249b1f8641b0a930c20c
nabrain <michal2002.60@gmail.com>
2025-09-08 16:23:38 +0200

    make fields NonNull

--- src/main/java/lule/dictionary/translations/data/request/CreateTranslationRequest.java
+++ src/main/java/lule/dictionary/translations/data/request/CreateTranslationRequest.java
@@ -1,14 +1,15 @@
 package lule.dictionary.translations.data.request;
 
 import lombok.Builder;
+import lombok.NonNull;
 import lule.dictionary.language.service.Language;
 
 @Builder
 public record CreateTranslationRequest(int documentId,
                                        int selectedWordId,
-                                       String username,
+                                       @NonNull String username,
                                        boolean isPhrase,
-                                       String targetWord,
-                                       Language sourceLanguage,
-                                       Language targetLanguage) {
+                                       @NonNull String targetWord,
+                                       @NonNull Language sourceLanguage,
+                                       @NonNull Language targetLanguage) {
 }
