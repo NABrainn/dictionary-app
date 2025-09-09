456c4833440868e91b17524751af7dfbbbde7e34
nabrain <michal2002.60@gmail.com>
2025-09-09 14:53:34 +0200

    create document sanitizer service for sanitization purposes

---
+++ src/main/java/lule/dictionary/documents/service/DocumentSanitizer.java
@@ -0,0 +1,16 @@
+package lule.dictionary.documents.service;
+
+import lombok.RequiredArgsConstructor;
+import lule.dictionary.documents.data.request.SanitizeNumberOfPagesRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.InvalidUrlException;
+
+@Service
+@RequiredArgsConstructor
+public class DocumentSanitizer {
+    public void sanitizeNumberOfPages(SanitizeNumberOfPagesRequest request) {
+        if(request.page() <= 0 || request.page() > request.numberOfPages()) {
+            throw new InvalidUrlException("Invalid url parameter provided");
+        }
+    }
+}
