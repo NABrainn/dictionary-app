bb95506edda354b9d4131beba0edc97ae2a65c8c
nabrain <michal2002.60@gmail.com>
2025-09-09 15:44:34 +0200

    delete

--- src/main/java/lule/dictionary/translations/data/request/GetWordsLearnedCountRequest.java
+++
@@ -1,9 +0,0 @@
-package lule.dictionary.translations.data.request;
-
-import lule.dictionary.language.service.Language;
-
-public record GetWordsLearnedCountRequest(String owner, Language targetLanguage) {
-    public static GetWordsLearnedCountRequest of(String username, Language language) {
-        return new GetWordsLearnedCountRequest(username, language);
-    }
-}
