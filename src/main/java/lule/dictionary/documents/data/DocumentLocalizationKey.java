130df0a11c208c135fed2ac9e8d1f11498f11598
nabrain <michal2002.60@gmail.com>
2025-09-10 15:22:33 +0200

    refactor the remaining controllers/services to use isolated localization

---
+++ src/main/java/lule/dictionary/documents/data/DocumentLocalizationKey.java
@@ -0,0 +1,19 @@
+package lule.dictionary.documents.data;
+
+public enum DocumentLocalizationKey {
+    AUTHOR,
+    WORDS_TOTAL,
+    NEW_WORDS,
+    TRANSLATIONS,
+    ADD_BOOK,
+    THERE_ARE_NO_DOCUMENTS_IN_THE_LIBRARY,
+    CLICK_HERE,
+    TO_ADD_YOUR_FIRST,
+    TITLE,
+    CONTENT,
+    IMPORT_BY_URL,
+    INSERT_MANUALLY,
+    SUBMIT,
+    SPACE_FOR_URL,
+    SPACE_FOR_CONTENT
+}
