cb7fe7de165cd2622312466da0339e96271c3478
nabrain <michal2002.60@gmail.com>
2025-09-14 22:00:44 +0200

    convert to stream based approach

--- src/main/java/lule/dictionary/localization/controller/LocalizationController.java
+++ src/main/java/lule/dictionary/localization/controller/LocalizationController.java
@@ -1,34 +1,31 @@
 package lule.dictionary.localization.controller;
 
 import jakarta.servlet.http.HttpSession;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lule.dictionary.language.service.Language;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.stream.Stream;
 @Controller
 @RequestMapping({"/localization", "/localization/"})
 @RequiredArgsConstructor
 public class LocalizationController {
 
     @GetMapping("/uiLanguage")
-    public ResponseEntity<String> setupInterfaceLanguage(@NonNull @RequestHeader("uiLanguage") String systemLanguage,
-                                                         HttpSession httpSession) {
-        Language sourceLanguage = parseHeader(systemLanguage);
-        httpSession.setAttribute("navbarLocalization", sourceLanguage);
-        return ResponseEntity.ok("System language info updated to: " + sourceLanguage);
-    }
-
-    private Language parseHeader(String sourceLanguageHeader) {
-        try {
-            String formattedHeader = sourceLanguageHeader.toUpperCase();
-            return Language.valueOf(formattedHeader);
-        } catch (IllegalArgumentException e) {
-            return Language.EN;
-        }
+    public void setupInterfaceLanguage(@RequestHeader("uiLanguage") String systemLanguage, HttpSession httpSession) {
+        Stream.of(systemLanguage)
+                .map(String::toUpperCase)
+                .map(header -> {
+                    try {
+                        return Language.valueOf(header);
+                    } catch (IllegalArgumentException e) {
+                        return Language.EN;
+                    }
+                })
+                .findFirst()
+                .ifPresent(language -> httpSession.setAttribute("navbarLocalization", language));
     }
 }
