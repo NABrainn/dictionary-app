e973d1f0b98e5778b7a88b89ef1908c62a4a79ad
nabrain <michal2002.60@gmail.com>
2025-09-15 11:50:19 +0200

    add new api for replacing endlines

--- src/main/java/lule/dictionary/stringUtil/service/PatternService.java
+++ src/main/java/lule/dictionary/stringUtil/service/PatternService.java
@@ -1,5 +1,6 @@
 package lule.dictionary.stringUtil.service;
 
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +11,9 @@ import java.util.regex.Pattern;
 public class PatternService {
     public String removeSpecialCharacters(String input) {
         return Pattern.compile("[^\\p{L}0-9 ]").matcher(input).replaceAll("");
+    }
+
+    public String replaceNewlinesWithSpaces(@NonNull String input) {
+        return Pattern.compile("\n+").matcher(input).replaceAll(" ");
     }
 }
