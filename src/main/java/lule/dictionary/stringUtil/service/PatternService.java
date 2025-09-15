07632042f5c327e76f782044d78ad0287c8c927e
nabrain <michal2002.60@gmail.com>
2025-09-15 13:11:09 +0200

    add email validity check api

--- src/main/java/lule/dictionary/stringUtil/service/PatternService.java
+++ src/main/java/lule/dictionary/stringUtil/service/PatternService.java
@@ -9,11 +9,17 @@ import java.util.regex.Pattern;
 @Service
 @RequiredArgsConstructor
 public class PatternService {
-    public String removeSpecialCharacters(String input) {
+    public String removeSpecialCharacters(@NonNull String input) {
         return Pattern.compile("[^\\p{L}0-9 ]").matcher(input).replaceAll("");
     }
 
     public String replaceNewlinesWithSpaces(@NonNull String input) {
         return Pattern.compile("\n+").matcher(input).replaceAll(" ");
+    }
+
+    public boolean isValidEmail(@NonNull String input) {
+        return Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+                .matcher(input)
+                .matches();
     }
 }
