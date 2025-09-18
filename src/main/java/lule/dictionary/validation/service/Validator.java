365af8b3117318fe7ae9012033db58a6656677c3
nabrain <michal2002.60@gmail.com>
2025-09-18 11:18:02 +0200

    refactor into result

--- src/main/java/lule/dictionary/validation/service/Validator.java
+++ src/main/java/lule/dictionary/validation/service/Validator.java
@@ -1,6 +1,9 @@
 package lule.dictionary.validation.service;
 
 import lombok.RequiredArgsConstructor;
+import lule.dictionary.result.data.Err;
+import lule.dictionary.result.data.Ok;
+import lule.dictionary.result.data.Result;
 import lule.dictionary.validation.data.Constraint;
 import lule.dictionary.validation.data.ValidationException;
 import org.springframework.stereotype.Service;
@@ -8,18 +11,21 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class Validator {
-    public void validate(List<Constraint> constraints) throws ValidationException {
-        Map<String, String> violations = constraints.stream()
+    public Result<?> validate(List<Constraint> constraints) {
+        Stream<Constraint> violationStream = constraints.stream()
                 .filter(constraint -> constraint.violationChecker().run())
-                .distinct()
-                .collect(Collectors.toUnmodifiableMap(Constraint::name, Constraint::message));
-        if(!violations.isEmpty()) {
-            throw new ValidationException(violations);
-        }
-    }
+                .distinct();
 
+        Map<String, String> violations = violationStream
+                .collect(Collectors.toUnmodifiableMap(Constraint::name, Constraint::message, (v1, v2) -> v1));
+
+        return violations.isEmpty()
+                ? Ok.empty()
+                : Err.of(new ValidationException(violations));
+    }
 }
