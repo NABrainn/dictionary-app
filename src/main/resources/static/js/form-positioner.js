d79bac413c77068384dcf8953eebbc6523b8bf5e
nabrain <michal2002.60@gmail.com>
2025-09-19 13:43:31 +0200

    add optional debug logging

--- src/main/resources/static/js/form-positioner.js
+++ src/main/resources/static/js/form-positioner.js
@@ -1,4 +1,5 @@
 window.formPositioner = window.formPositioner || {
+    debug: false,
     handleOverflow(form, sourceWordQuantity) {
         if(!form) {
             return
@@ -13,22 +14,30 @@ window.formPositioner = window.formPositioner || {
         //handle bottom overflow
         for(let i = 0; i < 2; i++) {
             if(form.getBoundingClientRect().bottom > height) {
-                console.log('overflow bottom', form.getBoundingClientRect())
+                if(this.debug) {
+                    console.log('overflow bottom', form.getBoundingClientRect())
+                }
                 form.style.top = ''
                 form.style.bottom = 32
             }
             //handle right overflow
             if(form.getBoundingClientRect().right > width) {
-                console.log('overflow right', form.getBoundingClientRect())
+                if(this.debug) {
+                    console.log('overflow right', form.getBoundingClientRect())
+                }
                 form.style.right = 8
             }
 
             //handle left underflow
             if(form.getBoundingClientRect().left < 0) {
-                console.log('overflow left', form.getBoundingClientRect())
+                if(this.debug) {
+                    console.log('overflow left', form.getBoundingClientRect())
+                }
                 form.style.right = -132
             }
-            console.log('finally', form.getBoundingClientRect())
+            if(this.debug) {
+                console.log('finally', form.getBoundingClientRect())
+            }
         }
     }
 }
