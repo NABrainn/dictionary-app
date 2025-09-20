06aa4ea1def33dcd33f1e1c688cc522ab65deda4
nabrain <michal2002.60@gmail.com>
2025-09-20 09:56:31 +0200

    change to string formatted size

--- src/main/resources/static/js/form-positioner.js
+++ src/main/resources/static/js/form-positioner.js
@@ -7,37 +7,47 @@ window.formPositioner = window.formPositioner || {
         const height = window.innerHeight;
         const width = window.innerWidth
         //reset position
-        form.style.top = 32
+        form.style.top = '32px'
         form.style.left = ''
         form.style.right = ''
         form.style.bottom = ''
+        if(this.debug) {
+            console.log('running checks...')
+        }
         //handle bottom overflow
-        for(let i = 0; i < 2; i++) {
-            if(form.getBoundingClientRect().bottom > height) {
-                if(this.debug) {
-                    console.log('overflow bottom', form.getBoundingClientRect())
-                }
-                form.style.top = ''
-                form.style.bottom = 32
-            }
-            //handle right overflow
-            if(form.getBoundingClientRect().right > width) {
-                if(this.debug) {
-                    console.log('overflow right', form.getBoundingClientRect())
-                }
-                form.style.right = 8
-            }
-
-            //handle left underflow
-            if(form.getBoundingClientRect().left < 0) {
-                if(this.debug) {
-                    console.log('overflow left', form.getBoundingClientRect())
-                }
-                form.style.right = -132
-            }
+        if(form.getBoundingClientRect().bottom > height) {
             if(this.debug) {
-                console.log('finally', form.getBoundingClientRect())
+                console.log('overflow bottom', form.getBoundingClientRect())
             }
+            form.style.top = ''
+            form.style.bottom = '32px'
+            if(this.debug) {
+                console.log('bottom', form.style.bottom)
+            }
+        }
+        //handle right overflow
+        if(form.getBoundingClientRect().right > width) {
+            if(this.debug) {
+                console.log('overflow right', form.getBoundingClientRect())
+            }
+            form.style.right = '8px'
+            if(this.debug) {
+                console.log('right', form.style.right)
+            }
+        }
+
+        //handle left underflow
+        if(form.getBoundingClientRect().left < 0) {
+            if(this.debug) {
+                console.log('overflow left', form.getBoundingClientRect())
+            }
+            form.style.right = '-132px'
+            if(this.debug) {
+                console.log('left', form.style.left)
+            }
+        }
+        if(this.debug) {
+            console.log('finally', form.style.left, form.style.right, form.style.top, form.style.bottom)
         }
     }
 }
