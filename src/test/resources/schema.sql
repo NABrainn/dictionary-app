240954fe137aee27bd2aa250de4b762ba271cf74
nabrain <michal2002.60@gmail.com>
2025-09-22 10:08:02 +0200

    update relation names and properties

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -8,45 +8,37 @@ CREATE TYPE dictionary.familiarity as ENUM ('UNKNOWN', 'RECOGNIZED', 'FAMILIAR',
 CREATE CAST (varchar AS dictionary.lang) WITH INOUT AS IMPLICIT;
 CREATE CAST (varchar AS dictionary.familiarity) WITH INOUT AS IMPLICIT;
 
-CREATE TABLE IF NOT EXISTS dictionary.user_profile_settings (
+CREATE TABLE IF NOT EXISTS dictionary.profile_settings (
 	settings_id						SERIAL PRIMARY KEY,
     source_lang  					dictionary.lang,
     target_lang  					dictionary.lang
 );
 
-CREATE TABLE IF NOT EXISTS dictionary.user_profiles (
+CREATE TABLE IF NOT EXISTS dictionary.users (
     username        				VARCHAR(50) NOT NULL UNIQUE,
     email           				VARCHAR(100) NOT NULL PRIMARY KEY,
     password        			    VARCHAR(500) NOT NULL,
-	settings_id						int REFERENCES dictionary.user_profile_settings(settings_id) ON UPDATE CASCADE ON DELETE CASCADE
+	settings_id						int REFERENCES dictionary.profile_settings(settings_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS dictionary.translations (
-    translations_id              	SERIAL PRIMARY KEY,
+    translation_id              	SERIAL PRIMARY KEY,
     source_words     				TEXT[] NOT NULL,
     target_word     				VARCHAR(200) NOT NULL,
     source_lang  					dictionary.lang NOT NULL,
     target_lang  					dictionary.lang NOT NULL,
-  	translation_owner				VARCHAR(50) NOT NULL references dictionary.user_profiles(username) ON UPDATE CASCADE ON DELETE CASCADE,
+  	translation_owner				VARCHAR(50) NOT NULL references dictionary.users(username) ON UPDATE CASCADE ON DELETE CASCADE,
   	familiarity						dictionary.familiarity NOT NULL
 );
-CREATE TABLE IF NOT EXISTS dictionary.imports (
-  	imports_id						SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS dictionary.documents (
+  	document_id				        SERIAL PRIMARY KEY,
   	title							VARCHAR (100) NOT NULL,
   	content							TEXT NOT NULL,
   	url								VARCHAR(200),
   	source_lang						dictionary.lang NOT NULL,
   	target_lang						dictionary.lang NOT NULL,
-  	import_owner					VARCHAR(50) NOT NULL references dictionary.user_profiles(username) ON UPDATE CASCADE ON DELETE CASCADE
+  	document_owner					VARCHAR(50) NOT NULL references dictionary.users(username) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS dictionary.imports_translations (
-	imports_id 						int REFERENCES dictionary.imports(imports_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    translations_id				    int REFERENCES dictionary.translations(translations_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    amount							int NOT NULL DEFAULT 1,
-
-    CONSTRAINT imports_translations_pkey PRIMARY KEY (imports_id, translations_id)
-);
-
--- INSERT INTO dictionary.user_profile_settings (settings_id, source_lang, target_lang) VALUES (1, 'EN', 'NO');
--- INSERT INTO dictionary.user_profiles (username, email, password, settings_id) VALUES ('nabrainn', 'na@brain.com', 'password', 1);
+-- INSERT INTO dictionary.profile_settings (settings_id, source_lang, target_lang) VALUES (1, 'EN', 'NO');
+-- INSERT INTO dictionary.users (username, email, password, settings_id) VALUES ('nabrainn', 'na@brain.com', 'password', 1);
