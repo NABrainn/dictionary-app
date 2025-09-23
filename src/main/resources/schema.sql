4bdae2b37b874f83c44b97d5937af093e5002606
nabrain <michal2002.60@gmail.com>
2025-09-23 11:55:31 +0200

    update schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,16 +12,15 @@ CREATE TABLE IF NOT EXISTS dictionary.profile_settings (
 	settings_id						SERIAL PRIMARY KEY,
     source_lang  					dictionary.lang,
     target_lang  					dictionary.lang,
-	ui_lang							dictionary.lang
+	ui_lang							dictionary.lang,
+	is_navbar_open					BOOLEAN DEFAULT false
 );
-
 CREATE TABLE IF NOT EXISTS dictionary.users (
     username        				VARCHAR(50) NOT NULL UNIQUE,
     email           				VARCHAR(100) NOT NULL PRIMARY KEY,
     password        			    VARCHAR(500) NOT NULL,
 	settings_id						int REFERENCES dictionary.profile_settings(settings_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
 CREATE TABLE IF NOT EXISTS dictionary.streaks (
 	day_count						int NOT NULL DEFAULT 0,
 	words_added_today				int	NOT NULL DEFAULT 0,
@@ -41,8 +40,13 @@ CREATE TABLE IF NOT EXISTS dictionary.translations (
   	familiarity						dictionary.familiarity NOT NULL,
   	is_phrase                       BOOLEAN default false
 );
+CREATE TABLE IF NOT EXISTS dictionary.flashcard_session (
+	session_id 						SERIAL PRIMARY KEY,
+	session_owner					VARCHAR(50) NOT NULL REFERENCES dictionary.users(username) ON UPDATE CASCADE ON DELETE CASCADE,
+	translation_id					int NOT NULL REFERENCES dictionary.translations(translation_id) ON UPDATE CASCADE ON DELETE CASCADE
+);
 CREATE TABLE IF NOT EXISTS dictionary.documents (
-  	document_id					    SERIAL PRIMARY KEY,
+  	document_id						SERIAL PRIMARY KEY,
   	title							VARCHAR (200) NOT NULL,
   	content							TEXT NOT NULL,
   	url								VARCHAR(200),
@@ -51,4 +55,3 @@ CREATE TABLE IF NOT EXISTS dictionary.documents (
   	document_owner					VARCHAR(50) NOT NULL references dictionary.users(username) ON UPDATE CASCADE ON DELETE CASCADE,
 	total_length					int NOT NULL
 );
-
