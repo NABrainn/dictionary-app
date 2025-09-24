0e53103413e12412562ce85b802b1b13c7d3936e
nabrain <michal2002.60@gmail.com>
2025-09-24 21:31:03 +0200

    rewrite to functional approach

--- src/main/java/lule/dictionary/translationFetching/service/LingvanexFetcher.java
+++ src/main/java/lule/dictionary/translationFetching/service/LingvanexFetcher.java
@@ -1,17 +1,15 @@
 package lule.dictionary.translationFetching.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lule.dictionary.language.service.Language;
 import lule.dictionary.language.service.LanguageHelper;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestClient;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -21,54 +19,49 @@ public class LingvanexFetcher implements TranslationFetcher {
 
     @Override
     public List<String> translate(Language sourceLanguage, Language targetLanguage, String targetWord) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
         String sourceLanguageCode = languageHelper.getCode(sourceLanguage);
         String targetLanguageCode = languageHelper.getCode(targetLanguage);
-        var formData = assembleFormData(targetWord, targetLanguageCode, sourceLanguageCode);
-        String response = RestClient.builder()
-                .build()
-                .post()
-                .uri(uriBuilder -> uriBuilder
-                        .scheme("https")
-                        .host("lingvanex.com")
-                        .path("/translation/translate")
-                        .build())
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(formData)
-                .retrieve()
-                .body(String.class);
-        if(response != null) {
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode json = mapper.readTree(response);
-            String result = json.get("result").toString();
-            return List.of(extractSourceWord(result));
-        }
-        return List.of();
-    }
-
-    private String extractSourceWord(String result) {
-        return result.substring(1, result.length() - 1).replace("-", " ");
-    }
-
-    private LinkedMultiValueMap<String, String> assembleFormData(String targetWord, String targetLanguageCode, String sourceLanguageCode) {
-        var formData = new LinkedMultiValueMap<String, String>();
-        formData.add("from_lang", getFromLangCode(targetLanguageCode));
-        formData.add("to", getToLangCode(sourceLanguageCode));
-        formData.add("text", targetWord.replace(" ", "-"));
-        formData.add("platform", "dp");
-        return formData;
-    }
-
-    private String getToLangCode(String sourceLanguageCode) {
-        return switch (sourceLanguageCode) {
-            case "en" -> "en_US";
-            default -> throw new IllegalStateException("Unexpected value: " + sourceLanguageCode);
-        };
-    }
-
-    private String getFromLangCode(String targetLanguageCode) {
-        return switch (targetLanguageCode) {
-            case "no" -> "no_NO";
-            default -> throw new IllegalStateException("Unexpected value: " + targetLanguageCode);
-        };
+        return Optional.ofNullable(RestClient.builder()
+                        .build()
+                        .post()
+                        .uri(uriBuilder -> uriBuilder
+                                .scheme("https")
+                                .host("lingvanex.com")
+                                .path("/translation/translate")
+                                .build())
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .body(Map.of(
+                                "from_lang", switch (targetLanguageCode) {
+                                    case "no" -> "no_NO";
+                                    case "en" -> "en_US";
+                                    case "it" -> "it_IT";
+                                    case "pl" -> "pl_PL";
+                                    default -> "";
+                                },
+                                "to", switch (sourceLanguageCode) {
+                                    case "no" -> "no_NO";
+                                    case "en" -> "en_US";
+                                    case "it" -> "it_IT";
+                                    case "pl" -> "pl_PL";
+                                    default -> "";
+                                },
+                                "text", targetWord.replace(" ", "-"),
+                                "platform", "dp"
+                        ))
+                        .retrieve()
+                        .body(String.class))
+                .map(jsonBlob -> {
+                    try {
+                        return mapper.readTree(jsonBlob);
+                    } catch (JsonProcessingException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .map(jsonNode -> jsonNode.get("result"))
+                .map(Objects::toString)
+                .map(json -> json.substring(1, json.length() - 1).replace("-", " "))
+                .map(List::of)
+                .orElse(List.of());
     }
 }
