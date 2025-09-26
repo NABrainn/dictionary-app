278184aea1f033deef2443906eac1bda4a984257
nabrain <michal2002.60@gmail.com>
2025-09-26 14:04:54 +0200

    adapt Result to Document type

--- src/main/java/lule/dictionary/documents/service/DocumentSanitizer.java
+++ src/main/java/lule/dictionary/documents/service/DocumentSanitizer.java
@@ -1,6 +1,7 @@
 package lule.dictionary.documents.service;
 
 import lombok.RequiredArgsConstructor;
+import lule.dictionary.documents.data.entity.Document;
 import lule.dictionary.documents.data.request.SanitizeNumberOfPagesRequest;
 import lule.dictionary.result.data.Err;
 import lule.dictionary.result.data.Ok;
@@ -11,9 +12,9 @@ import org.springframework.web.util.InvalidUrlException;
 @Service
 @RequiredArgsConstructor
 public class DocumentSanitizer {
-    public Result<?> validateNumberOfPages(SanitizeNumberOfPagesRequest request) {
+    public Result<Document> validateNumberOfPages(SanitizeNumberOfPagesRequest request) {
         return request.page() <= 0 || request.page() > request.numberOfPages() ?
                 Err.of(new InvalidUrlException("Invalid url parameter provided")) :
-                Ok.empty();
+                Ok.of(request.document());
     }
 }
